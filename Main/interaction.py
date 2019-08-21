758ede066de1f37d10d1cef04fb2be867d927646
luckky <guodashun@163.com>
2019-08-22 01:06:19 +0800

    fix name bug

--- Main/interaction.py
+++ Main/interaction.py
@@ -1,25 +1,25 @@
-import cmdItf
+import cmdIf
 from PyQt5.QtCore import QObject, pyqtSlot
 
 class Interaction(QObject):
 	@pyqtSlot()
 	def cmdInit(self):
-		cmdItf.init()
+		cmdIf.init()
 
 	@pyqtSlot(int, int, int, int, bool, int, bool, bool, int)
 	def updateCommands(self, rbtID, vx, vy, vr, dribble, dribbleLevel, mode, shoot, power):
 		if dribble:
-			cmdItf.setSpeed(rbtID, dribbleLevel, vx, vy, vr)
+			cmdIf.setSpeed(rbtID, dribbleLevel, vx, vy, vr)
 		else:
-			cmdItf.setSpeed(rbtID, 0, vx, vy, vr)
+			cmdIf.setSpeed(rbtID, 0, vx, vy, vr)
 		if not shoot:
-			cmdItf.setKick(rbtID, 0, 0)
+			cmdIf.setKick(rbtID, 0, 0)
 		else:
 			if not mode:
-				cmdItf.setKick(rbtID, power, 0)
+				cmdIf.setKick(rbtID, power, 0)
 			else:
-				cmdItf.setKick(rbtID, 0, power)
+				cmdIf.setKick(rbtID, 0, power)
 	
 	@pyqtSlot()
 	def sendCommands(self):
-		cmdItf.sendCommands()
+		cmdIf.sendCommands()
