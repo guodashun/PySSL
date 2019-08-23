a45b3a7d60bb90c880c09e7213bbc665e4f2d43f
luckky <guodashun@163.com>
2019-08-23 21:21:47 +0800

    add visionIf

--- Main/README.md
+++ Main/README.md
@@ -2,7 +2,8 @@
 ```
 $ python setup.py bdist_wheel
 $ cd dist
-$ pip install PySSL-0.0.1-py3-none-any.whl
+$ pip install PySSL-0.0.2-py3-none-any.whl
+$ pip install pyqt5
 ```
 
 ### RUN
--- Main/setup.py
+++ Main/setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
 	name = 'PySSL',
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'Basic libraries for SSL',
-	packages = ['cmdIf'],
+	packages = ['cmdIf', 'visionIf'],
 	package_data = {
-		'cmdIf' : ['cmdIf.pyd', 'boost_python37-vc140-mt-x64-1_67.dll']
+		'cmdIf' : ['cmdIf.pyd', 'boost_python37-vc140-mt-x64-1_67.dll'],
+		'visionIf' : ['visionIf.pyd', 'boost_python37-vc140-mt-x64-1_67.dll']
 	}
 )
